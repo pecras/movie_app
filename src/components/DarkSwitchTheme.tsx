f76f66820661c5c7c4613e55a555f78ac239f71c
pecras <pecras1@hotmail.com>
2024-04-11 06:19:41 -0300

    feat:theme

--- src/components/DarkSwitchTheme.tsx
+++ src/components/DarkSwitchTheme.tsx
@@ -1,15 +1,25 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MdLightMode,MdDarkMode } from 'react-icons/md'
 import { useTheme } from 'next-themes'
 
 
 export default function DarkSwitchTheme() {
 const{theme,setTheme,systemTheme}=useTheme()
+const [mounted,setMounted] = useState(false);
 const curretTheme =theme === 'system'? systemTheme:theme;
+useEffect(() => {
+  setMounted(true)
+}, [])
+
+
+
+
   return (
     <div>
-        {curretTheme==='dark'?(<MdLightMode/>):(<MdDarkMode/>)
+        {mounted && curretTheme==='dark'?
+        (<MdLightMode className='w-8 h-8 border-2 rounded-md  border-blue-200 hover:bg-amber-600  hover:border-amber-400' onClick={()=>{setTheme('light')}}/>):
+        (<MdDarkMode className='w-8 h-8 border-2 rounded-md  border-blue-200 hover:bg-amber-600  hover:border-amber-400' onClick={()=>{setTheme('dark')}}/>)
         }
     </div>
   )
--- src/themes/Themes.tsx
+++ src/themes/Themes.tsx
@@ -8,7 +8,11 @@ children:React.ReactNode
 export default function Themes({children}:Propsthems) {
   return (
 <ThemeProvider defaultTheme="system" attribute="class">
+  <div className="text-gray-700 bg-white dark:text-teal-200 dark:bg-black min-h-screen select-none transition-colors duration-300" >
+    
+  
     {children}
+    </div>
 </ThemeProvider>
   )
 }
