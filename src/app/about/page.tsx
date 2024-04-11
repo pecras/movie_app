3fad1c3964aa0f2ae2d10907fab7597fd483e23c
pecras <pecras1@hotmail.com>
2024-04-11 06:35:03 -0300

    feat:about Page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='flex flex-col justify-center align-middle text-center m-4'>
+        <h1 className='mb-8 font-medium text-amber-500'>IMDb</h1>
+        <h3 >
+        Bem-vindo ao nosso catálogo de filmes variados, onde a diversidade é a estrela! Em nosso mundo cinematográfico, exploramos uma ampla gama de gêneros, desde drama emocionante até comédias hilárias, passando por ação empolgante e suspense arrepiante.
+
+Nossa missão é proporcionar uma experiência cinematográfica enriquecedora para todos os gostos e idades. Com uma seleção cuidadosamente curada de filmes de todo o mundo, queremos conectar você com histórias emocionantes, personagens cativantes e mundos fascinantes.
+
+Em nosso catálogo, você encontrará clássicos do cinema, lançamentos recentes, obras-primas aclamadas pela crítica e gemas escondidas que merecem ser descobertas. Nosso objetivo é oferecer uma jornada cinematográfica única a cada visita, onde você possa se perder em histórias que o inspiram, o fazem rir, chorar e refletir.
+
+Então, junte-se a nós enquanto exploramos o vasto universo do cinema. Permita-se ser transportado para lugares distantes, conhecer personagens inesquecíveis e vivenciar emoções intensas. Estamos aqui para alimentar sua paixão pelo cinema e ajudá-lo a descobrir novos favoritos. Prepare a pipoca, escolha seu filme e deixe a magia do cinema começar!
+        </h3>
+    </div>
+  )
+}
